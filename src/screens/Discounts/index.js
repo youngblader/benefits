3b00b80b8e2771e2480e6f03e2669d7c92be137b
Eugene <y.zorych@tapston.com>
2022-06-01 23:22:32 +0300

    refactor imports

--- src/screens/Discounts/index.js
+++ src/screens/Discounts/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
 import Discounts from './Discounts';
 
 import data from '../../config/data.json'
