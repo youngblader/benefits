0ced73cf607e87d275071f3cbd579f50510d8242
Eugene <y.zorych@tapston.com>
2022-06-13 14:42:45 +0300

    refactor view benefits

--- src/screens/Discounts/index.js
+++ src/screens/Discounts/index.js
@@ -1,10 +1,10 @@
 import React, { useState, useRef } from 'react';
 import Discounts from './Discounts';
 
-import data from '../../config/data.json'
+const startNavigationItem =  { id: 0, title: 'Все скидки' };
 
 const DiscountsContainer = props => {
-  const [selectCategory, setSelectCategory] = useState(data.categories[0]);
+  const [selectCategory, setSelectCategory] = useState(startNavigationItem || {});
   const [activeCategory, setActiveCategory] = useState(false);
 
   const scrollRef = useRef();
@@ -13,14 +13,20 @@ const DiscountsContainer = props => {
     props.navigation.navigate('DetailsBenefitInfo', { benefit: el });
   };
 
-  const modifiedData = data => {
-    let currentItems = [];
-    let moreItems = [];
-
-    data?.reduce((acc, curr, index) => 
-      index <= 3 ? acc + currentItems.push(curr) : acc + moreItems.push(curr), 0);
-  
-    return ({ currentItems, moreItems })
+  const modifiedData = data => {  
+    return data?.reduce((acc, curr, index) => {       
+      if (index <= 3) {
+        return {
+          currentItems: [...acc.currentItems, curr], 
+          moreItems: [...acc.moreItems]
+        }
+      } else {
+        return {
+          currentItems: [...acc.currentItems], 
+          moreItems: [...acc.moreItems, curr]
+        }
+      }
+    }, { currentItems: [], moreItems: [] });
   }
 
   const handleOnPressCategory = (item, index, navigate = false) => {
@@ -45,7 +51,6 @@ const DiscountsContainer = props => {
       modifiedData={modifiedData}
       handleOnPressCategory={handleOnPressCategory}
       handleSelectBenefit={handleSelectBenefit}
-      setActiveCategory={setActiveCategory}
     />
   );
 }
